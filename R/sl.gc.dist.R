cda3c0a40a974f006824b0fce304c99ba8881a86
helgegoessling <helge.goessling@awi.de>
2017-05-03 21:29:52 +0200

    Added another mode to sl.gc.dist so that computing the distance from one point to many other points simultaneously is substantially sped up.

--- R/sl.gc.dist.R
+++ R/sl.gc.dist.R
@@ -1,13 +1,29 @@
 sl.gc.dist <-
-function(lon,lat,Rsphere=1) {
-	L = length(lon) - 1
-	pointdist = rep(NA,L)
-	for (l in 1:L) {
-		alpha = lon[l] + 90
-		beta = 90 - lat[l]
+function(lon,lat,Rsphere=1,sequential=TRUE) {
+	
+	if (sequential) {
+	
+		L = length(lon) - 1
+		pointdist = rep(NA,L)
+		for (l in 1:L) {
+			alpha = lon[l] + 90
+			beta = 90 - lat[l]
+			gamma = 0
+			rot.res = sl.rot(lon[l+1],lat[l+1],alpha,beta,gamma)
+			pointdist[l] = (90 - rot.res$lat) * pi * Rsphere / 180
+		}
+		
+	} else {
+		
+		L = length(lon) - 1
+		alpha = lon[1] + 90
+		beta = 90 - lat[1]
 		gamma = 0
-		rot.res = sl.rot(lon[l+1],lat[l+1],alpha,beta,gamma)
-		pointdist[l] = (90 - rot.res$lat) * pi * Rsphere / 180
+		rot.res = sl.rot(lon[2:(L+1)],lat[2:(L+1)],alpha,beta,gamma)
+		pointdist = (90 - rot.res$lat) * pi * Rsphere / 180
+		
 	}
+	
 	return(pointdist)
+	
 }
