419b44c8a2c8318ac2d611573f53936a23a545bf
helgegoessling <helge.goessling@awi.de>
2017-05-19 14:42:31 +0200

    Added function sl.plot.points (including documentation) to naturally extend the plotting capabilities.

---
+++ R/sl.plot.points.R
@@ -0,0 +1,53 @@
+sl.plot.points <-
+function (plot.init.res,lon,lat,col="black",pch=1,ignore.visibility=FALSE) {
+	
+	projection = plot.init.res$projection
+	
+	if (projection == "platon" || projection == "3D") {
+		
+		npir = 1
+		repeat {
+			pir = plot.init.res[[npir]]
+			if (!is.list(pir)) {return()}
+			if (is.null(pir$projection)) {return()}
+			sl.plot.points(pir,lon,lat,col,pch,ignore.visibility)
+			npir = npir + 1
+		}
+		
+	}
+			
+	vsr.res = sl.vis.shift.rot(plot.init.res,lon,lat)
+	x = vsr.res$x
+	y = vsr.res$y
+	rot.lon = vsr.res$rot.lon
+	rot.lat = vsr.res$rot.lat
+	visible = vsr.res$visible
+	
+	if (ignore.visibility) {
+		visible[] = TRUE
+	}
+	
+	xshift = plot.init.res$xshift
+	yshift = plot.init.res$yshift
+	
+	np = length(lon)
+	if (sum(visible) < np) {
+		if (length(col) > 1) {
+			if (length(col) < np) {
+				col = rep(col,np%/%length(col)+1)
+			}
+			col = col[(1:np)[visible]]
+		}
+		if (length(pch) > 1) {
+			if (length(pch) < np) {
+				pch = rep(pch,np%/%length(pch)+1)
+			}
+			pch = pch[(1:np)[visible]]
+		}
+		x = x[visible]
+		y = y[visible]
+	}
+	
+	points(x=x+xshift,y=y+yshift,col=col,pch=pch)
+	
+}
