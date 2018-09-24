afe4954b858c145cb5402cea62c81d177691e209
helgegoessling <helge.goessling@awi.de>
2018-09-24 17:34:44 +0200

    Implemented automatic label placement in sl.plot.colbar

--- R/sl.plot.colbar.R
+++ R/sl.plot.colbar.R
@@ -1,9 +1,14 @@
 sl.plot.colbar <-
-function (colbar,breaks,vertical=TRUE,labels.at=rep(TRUE,length(breaks)),labels=as.character(signif(breaks,3)),labels.side="bottom",labels.cex=1,ticks.length=1,ticks.mirrored=FALSE,ratio=.1,triag.ends=FALSE,device="pdf",do.init.device=TRUE,do.close.device=do.init.device,file.name=paste0("~/sl.plot.colbar.",device),width=6) {
-	
-	if (length(breaks) != length(colbar) - 1) {
-		stop("'breaks' must be shorter by exactly one element compared to 'colbar'")
-	}
+function (colbar,breaks,vertical=TRUE,labels.at=NULL,labels.num=min(length(breaks),9),labels=as.character(signif(breaks,3)),labels.side="bottom",labels.cex=1,ticks.length=1,ticks.mirrored=FALSE,ratio=.1,triag.ends=FALSE,device="pdf",do.init.device=TRUE,do.close.device=do.init.device,file.name=paste0("~/sl.plot.colbar.",device),width=6) {
+  
+  if (length(breaks) != length(colbar) - 1) {
+    stop("'breaks' must be shorter by exactly one element compared to 'colbar'")
+  }
+  
+  if (is.null(labels.at)) {
+    labels.at = rep(FALSE,length(breaks))
+    labels.at[round(seq(1,length(breaks),(length(breaks)-1)/(labels.num-1)))] = TRUE
+  }
 	
   if (do.init.device) {
     dev.fun = match.fun(device,descend=FALSE)
