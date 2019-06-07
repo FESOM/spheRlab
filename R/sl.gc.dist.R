7e082595486b2df04d5c78aff132b98a94882613
helgegoessling <helge.goessling@awi.de>
2019-06-07 16:06:20 +0200

    Extended sl.gc.dist() so that two vectors (two-column matrices) can be provided as input for each of lon and lat. Distances are then computed for each pair of points

--- R/sl.gc.dist.R
+++ R/sl.gc.dist.R
@@ -1,29 +1,47 @@
 sl.gc.dist <-
-function(lon,lat,Rsphere=1,sequential=TRUE) {
-	
-	if (sequential) {
-	
-		L = length(lon) - 1
-		pointdist = rep(NA,L)
-		for (l in 1:L) {
-			alpha = lon[l] + 90
-			beta = 90 - lat[l]
-			gamma = 0
-			rot.res = sl.rot(lon[l+1],lat[l+1],alpha,beta,gamma)
-			pointdist[l] = (90 - rot.res$lat) * pi * Rsphere / 180
-		}
-		
-	} else {
-		
-		L = length(lon) - 1
-		alpha = lon[1] + 90
-		beta = 90 - lat[1]
-		gamma = 0
-		rot.res = sl.rot(lon[2:(L+1)],lat[2:(L+1)],alpha,beta,gamma)
-		pointdist = (90 - rot.res$lat) * pi * Rsphere / 180
-		
-	}
-	
-	return(pointdist)
-	
-}
+  function(lon,lat,Rsphere=1,sequential=TRUE) {
+    
+    if (is.vector(lon)) {
+      
+      if (sequential) {
+        
+        L = length(lon) - 1
+        pointdist = rep(NA,L)
+        for (l in 1:L) {
+          alpha = lon[l] + 90
+          beta = 90 - lat[l]
+          gamma = 0
+          rot.res = sl.rot(lon[l+1],lat[l+1],alpha,beta,gamma)
+          pointdist[l] = (90 - rot.res$lat) * pi * Rsphere / 180
+        } 
+        
+      } else {
+        
+        L = length(lon) - 1
+        alpha = lon[1] + 90
+        beta = 90 - lat[1]
+        gamma = 0
+        rot.res = sl.rot(lon[2:(L+1)],lat[2:(L+1)],alpha,beta,gamma)
+        pointdist = (90 - rot.res$lat) * pi * Rsphere / 180
+        
+      }
+      
+    } else {
+      
+      if (ncol(lon) != 2) {stop("If 'lon' and 'lat' are no vectors,
+	                              they must be 2-column matrices or data frames")}
+      L = length(lon[,1])
+      pointdist = rep(NA,L)
+      for (l in 1:L) {
+        alpha = lon[l,1] + 90
+        beta = 90 - lat[l,1]
+        gamma = 0
+        rot.res = sl.rot(lon[l,2],lat[l,2],alpha,beta,gamma)
+        pointdist[l] = (90 - rot.res$lat) * pi * Rsphere / 180
+      }
+      
+    }
+    
+    return(pointdist)
+    
+  }
