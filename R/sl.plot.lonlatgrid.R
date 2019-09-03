9387b117b28f33aeecb8b39a60e8e9ab92ba8e0c
helgegoessling <helge.goessling@awi.de>
2019-09-03 20:59:58 +0200

    Added arguments 'lon.range' and 'lat.range' to sl.plot.lonlatgrid(): 'If grid lines are very dense, this function can be quite slow even if only a small map section is actually plotted. In such cases, the arguments lon.range and lat.range can be used to considerably speed up the process.'

--- R/sl.plot.lonlatgrid.R
+++ R/sl.plot.lonlatgrid.R
@@ -1,5 +1,5 @@
 sl.plot.lonlatgrid <-
-function (plot.init.res,lon.0=0,lat.0=0,lon.distance=NULL,lat.distance=NULL,autodensity.f=1,pole.hole=FALSE,precision=1,
+function (plot.init.res,lon.0=0,lat.0=0,lon.distance=NULL,lat.distance=NULL,lon.range=c(-180,180),lat.range=c(-90,90),autodensity.f=1,pole.hole=FALSE,precision=1,
           lty=1,lwd=.25,col="black",labels=FALSE,labels.lat.every=3,labels.lat.first=ceiling(labels.lat.every/2),
           labels.lat.offsetlatlon=c(0,0),labels.lon.every=labels.lat.every,labels.lon.first=ceiling(labels.lon.every/2),
           labels.lon.offsetlatlon=c(0,0),labels.col="grey",labels.round.digits=NULL,labels.cex=1) {
@@ -50,43 +50,76 @@ function (plot.init.res,lon.0=0,lat.0=0,lon.distance=NULL,lat.distance=NULL,auto
 	lat.distance = abs(lat.distance)
 	
 	lon.start = lon.0
-	while (lon.start >= lon.distance - 180) {
+	while (lon.start < lon.range[1]) {
+	  lon.start = lon.start + lon.distance
+	}
+	while (lon.start >= lon.distance + lon.range[1]) {
 		lon.start = lon.start - lon.distance
 	}
-	lons = seq(lon.start,180,lon.distance)
-	Nlon = length(lons)
-	if (lons[Nlon] == 180 && lons[1] == -180) {
-		Nlon = Nlon - 1
-		lons = lons[1:Nlon]
+	if (lon.start > lon.range[2]) {
+	  lons = NULL
+	  Nlon = 0
+	} else {
+	  lons = seq(lon.start,lon.range[2],lon.distance)
+	  Nlon = length(lons)
+	  if (lons[Nlon] == 180 && lons[1] == -180) {
+		  Nlon = Nlon - 1
+		  lons = lons[1:Nlon]
+	  }
 	}
 	
 	lat.start = lat.0
-	while (lat.start > lat.distance - 90) {
+	while (lat.start < lat.range[1]) {
+	  lat.start = lat.start + lat.distance
+	}
+	while (lat.start >= lat.distance + lat.range[1]) {
 		lat.start = lat.start - lat.distance
 	}
-	lats = seq(lat.start,90,lat.distance)
-	Nlat = length(lats)
-	if (lats[Nlat] == 90) {
-		Nlat = Nlat - 1
-		lats = lats[1:Nlat]
+	if (lat.start > lat.range[2]) {
+	  Nlat = 0
+	} else {
+	  lats = seq(lat.start,lat.range[2],lat.distance)
+	  Nlat = length(lats)
+	  if (lats[Nlat] == 90) {
+		  Nlat = Nlat - 1
+		  lats = lats[1:Nlat]
+	  }
+	  if (lats[1] == -90) {
+	    lats = lats[2:Nlat]
+	    Nlat = Nlat - 1
+	  }
+	}
+	
+	if (Nlat+Nlon == 0) {
+	  warning("no lines to be drawn by sl.plot.lonlatgrid")
+	  return(NULL)
 	}
 	
 	lines.lonlatgrid = list()
-	lon = seq(-180,180,precision)
-	if (lon[length(lon)] != 180) {lon = c(lon,180)}
-	for (n in 1:Nlat) {
-		lines.lonlatgrid[[n]] = list(lon=lon,lat=rep(lats[n],length(lon)))
+	
+	if (Nlat > 0) {
+	  lon = seq(lon.range[1],lon.range[2],precision)
+	  if (lon[length(lon)] != lon.range[2]) {lon = c(lon,lon.range[2])}
+	  for (n in 1:Nlat) {
+		  lines.lonlatgrid[[n]] = list(lon=lon,lat=rep(lats[n],length(lon)))
+	  }
 	}
 	
-	if (pole.hole) {
-		lat.minmax = c(lats[1],lats[Nlat])
-	} else {
-		lat.minmax = c(-90,90)
-	}
-	lat = seq(lat.minmax[1],lat.minmax[2],precision)
-	if (lat[length(lat)] != lat.minmax[2]) {lat = c(lat,lat.minmax[2])}
-	for (n in ((1:Nlon)+Nlat)) {
-		lines.lonlatgrid[[n]] = list(lon=rep(lons[n-Nlat],length(lat)),lat=lat)
+	if (Nlon > 0) {
+	  lat.minmax = lat.range
+	  if (pole.hole) {
+	    if (lats[1] - lat.distance <= -90) {
+		    lat.minmax[1] = lats[1]
+	    }
+	    if (lats[Nlat] + lat.distance >= 90) {
+	      lat.minmax[2] = lats[Nlat]
+	    }
+	  }
+	  lat = seq(lat.minmax[1],lat.minmax[2],precision)
+	  if (lat[length(lat)] != lat.minmax[2]) {lat = c(lat,lat.minmax[2])}
+	  for (n in ((1:Nlon)+Nlat)) {
+		  lines.lonlatgrid[[n]] = list(lon=rep(lons[n-Nlat],length(lat)),lat=lat)
+	  }
 	}
 	
 	for (n in 1:(Nlat+Nlon)) {
@@ -94,6 +127,10 @@ function (plot.init.res,lon.0=0,lat.0=0,lon.distance=NULL,lat.distance=NULL,auto
 	}
 	
 	if (labels) {
+	  if (Nlon == 0 || Nlat == 0) {
+	    warning("labels not possible (no longitudes or no latitudes are drawn)")
+	    return(NULL)
+	  }
 	  lons.medians = c()
 	  for (i in 1:Nlon) {
 	    lons.medians = c(lons.medians, sl.p2p(lons[i],0,lons[i%%Nlon + 1],0,frac=0.5)$lon)
