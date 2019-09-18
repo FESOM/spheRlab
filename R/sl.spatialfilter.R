2ef495b317f89b62cf6ed5d0a3109497de1ffd06
helgegoessling <helge.goessling@awi.de>
2019-09-18 15:57:41 +0200

    Introduced argument 'na.rm' in sl.spatialfilter() to enable removal of NA values so that 'NA-spread' associated with spatial filtering can be turned into 'NA-decay'

--- R/sl.spatialfilter.R
+++ R/sl.spatialfilter.R
@@ -1,5 +1,5 @@
 sl.spatialfilter <-
-function (num,sf.gw.res) {
+function (num,sf.gw.res,na.rm=FALSE) {
 	
 	w = sf.gw.res$filter_weights
 	s = sf.gw.res$src_address
@@ -8,15 +8,22 @@ function (num,sf.gw.res) {
 	Ni = length(w)
 	
 	num.filtered = rep(NA,N)
-	i = 1
 	for (d.ind in 1:N) {
 		
-		nu = 0
-		while (i <= Ni && d[i] == d.ind) {
-			nu = nu + w[i]*num[s[i]]
-			i = i + 1
+	  inds.tmp = which(d==d.ind)
+	  num.tmp = num[s[inds.tmp]]
+	  w.tmp = w[inds.tmp]
+		if (na.rm) {
+		  na.tmp = is.na(num.tmp)
+		  if (sum(na.tmp) > 0) {
+		    if (sum(na.tmp) == length(inds.tmp)) {next}
+		    w.tmp.sum = sum(w.tmp[!na.tmp])
+		    if (w.tmp.sum == 0) {next}
+		    w.tmp = w.tmp / w.tmp.sum
+		    num.tmp[na.tmp] = 0
+		  }
 		}
-		num.filtered[d.ind] = nu
+	  num.filtered[d.ind] = sum(w.tmp*num.tmp)
 		
 	}
 	
