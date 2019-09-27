40d8f952a94c8eb9be6da58b7e467f79995c38d7
helgegoessling <helge.goessling@awi.de>
2019-09-27 21:47:07 +0200

    Updated sl.xyz2lonlat() and sl.lonlat2xyz() so that they can take multiple points simultaneously as arguments (allowing vectorization).

--- R/sl.lonlat2xyz.R
+++ R/sl.lonlat2xyz.R
@@ -1,14 +1,25 @@
 sl.lonlat2xyz <-
-function(lonlatvec) {
-	if (length(lonlatvec) != 2) {
-		stop("lonlatvec must be a lon-lat vector of length 2!")
+function(lonlatvec=NULL, lon=NULL, lat=NULL) {
+  
+	if (!is.null(lonlatvec)) {
+	  if (length(lonlatvec) != 2) {
+		  stop("lonlatvec must be a lon-lat vector of length 2 (or NULL)")
+	  }
+	  lon = lonlatvec[1]
+	  lat = lonlatvec[2]
 	}
-	rad = pi / 180
-	lonlatvec = lonlatvec * rad
-	lon = lonlatvec[1]
-	lat = lonlatvec[2]
+  
+	lon = pi / 180 * lon
+	lat = pi / 180 * lat
+	
 	x = cos(lat) * cos(lon)
 	y = cos(lat) * sin(lon)
 	z = sin(lat)
-	return(c(x,y,z))
+	
+	if (!is.null(lonlatvec)) {
+	  return(c(x,y,z))
+	} else {
+	  return(list(x=x,y=y,z=z))
+	}
+	
 }
--- R/sl.xyz2lonlat.R
+++ R/sl.xyz2lonlat.R
@@ -1,9 +1,27 @@
 sl.xyz2lonlat <-
-function(xyzvec) {
-	if (length(xyzvec) != 3) {stop("xyzvec must be a cartesian vector of length 3!")}
-	xyzvec = xyzvec / sl.cart.dist(xyzvec)
+function(xyzvec=NULL, x=NULL, y=NULL, z=NULL) {
+  
+	if (!is.null(xyzvec)) {
+	  if (length(xyzvec) != 3) {stop("xyzvec must be a cartesian vector of length 3 (or NULL)")}
+	  xyzvec = xyzvec / sqrt(sum(xyzvec^2))
+	  x = xyzvec[1]
+	  y = xyzvec[2]
+	  z = xyzvec[3]
+	} else {
+	  len = sqrt(x^2 + y^2 + z^2)
+	  x = x / len
+	  y = y / len
+	  z = z / len
+	}
+  
 	rad = pi / 180
-	lon = atan2(xyzvec[2],xyzvec[1]) / rad
-	lat = asin(xyzvec[3]) / rad
-	return(c(lon,lat))
+	lon = atan2(y,x) / rad
+	lat = asin(z) / rad
+	
+	if (!is.null(xyzvec)) {
+	  return(c(lon,lat))
+	} else {
+	  return(list(lon=lon,lat=lat))
+	}
+	
 }
