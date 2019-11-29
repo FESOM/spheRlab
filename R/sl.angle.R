4de76a9a38503f3e6553eab294275385fbd1fe76
helgegoessling <helge.goessling@awi.de>
2019-11-29 13:01:51 +0100

    Added argument 'left' to sl.angle() so that the function can be used in such a way that the directional information is preserved, instead of always just returning the smaller angle below 180 degree / below pi

--- R/sl.angle.R
+++ R/sl.angle.R
@@ -1,22 +1,20 @@
 sl.angle <-
-function(lon,lat,return.degree=FALSE) {
+function(lon,lat,return.degree=FALSE,left=FALSE) {
+  
 	if (length(lon) != 3 || length(lat) != 3) {
 		stop("lon and lat must be vectors of length 3!")
 	}
 	alpha = lon[2] + 90
 	beta = 90 - lat[2]
 	gamma = 0
-	rot.res = sl.rot(lon,lat,alpha,beta,gamma,return.xyz=TRUE)
-	x = rot.res$x
-	y = rot.res$y
-	a = c(x[2],y[2])
-	b = c(x[1],y[1])
-	c = c(x[3],y[3])
-	ab = b - a
-	ac = c - a
-	options(warn=-1)
-	ang = as.numeric(acos((ab/sqrt(sum(ab^2))) %*% (ac/sqrt(sum(ac^2)))))
-	options(warn=1)
-	if (return.degree) {ang=ang*360/2/pi}
+	
+	rot.res = sl.rot(lon,lat,alpha,beta,gamma,return.xyz=FALSE)
+	ang = rot.res$lon[1] - rot.res$lon[3]
+	if (ang < 0) {ang = ang + 360}
+	if (ang >= 360) {ang = ang - 360}
+	if (!left && ang > 180) {ang = 360 - ang}
+	if (!return.degree) {ang = ang/360*2*pi}
+	
 	return(ang)
+	
 }
