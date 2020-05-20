6bf348b699f8aa52e9a9518e80c2d78961915744
helgegoessling <helge.goessling@awi.de>
2020-05-20 18:59:47 +0200

    Enhancements and bug fixes in sl.gc.dist and sl.finddist

--- R/sl.finddist.R
+++ R/sl.finddist.R
@@ -2,26 +2,45 @@ sl.finddist <-
 function(lon1,lat1,lon2=NULL,lat2=NULL,fun=min,return.vectors=TRUE,return.alldist=FALSE,exclude.zero=FALSE,return.degree=FALSE,Rsphere=1,reduce.memory=FALSE) {
   
   M = length(lon1)
+  
+  lon1.x = pi / 180 * lon1
+  lat1.x = pi / 180 * lat1
+  x1 = cos(lat1.x)*cos(lon1.x)
+  y1 = cos(lat1.x)*sin(lon1.x)
+  z1 = sin(lat1.x)
   if (is.null(lon2)) {
     lon2 = lon1
     lat2 = lat1
+    x2 = x1
+    y2 = y1
+    z2 = z1
+  } else {
+    lon2.x = pi / 180 * lon2
+    lat2.x = pi / 180 * lat2
+    x2 = cos(lat2.x)*cos(lon2.x)
+    y2 = cos(lat2.x)*sin(lon2.x)
+    z2 = sin(lat2.x)
   }
   N = length(lon2)
   
-  if (!reduce.memory) {alldist = matrix(NA, nrow=M, ncol=N)}
+  if (reduce.memory) {
+    if (return.alldist) {stop("distance matrix can not be returned with 'reduce.memory=TRUE'")}
+  } else {
+    alldist = matrix(NA, nrow=M, ncol=N)
+  }
   funres12.ind = numeric(M)
   funres12.dist = numeric(M)
   funres21.ind = numeric(N)
   funres21.dist = numeric(N)
   
   if (return.degree) {
-    fac = 360 / 2 / pi / Rsphere
+    fac = 360 / 2 / pi
   } else {
-    fac = 1 / Rsphere
+    fac = Rsphere
   }
   
 	for (m in 1:M) {
-	  dists = sl.gc.dist(c(lon1[m],lon2), c(lat1[m],lat2), sequential = FALSE) * fac
+	  dists = sl.gc.dist(x=c(x1[m],x2), y=c(y1[m],y2), z=c(z1[m],z2), sequential = FALSE) * fac
 	  dists[lon2==lon1[m] & lat2==lat1[m]] = 0
 	  if (exclude.zero) {dists[dists==0] = NA}
 	  if (!reduce.memory) {alldist[m,] = dists}
@@ -36,7 +55,7 @@ function(lon1,lat1,lon2=NULL,lat2=NULL,fun=min,return.vectors=TRUE,return.alldis
     }
   } else {
     for (n in 1:N) {
-      dists = sl.gc.dist(c(lon2[n],lon1), c(lat2[n],lat1), sequential = FALSE) * fac
+      dists = sl.gc.dist(x=c(x2[n],x1), y=c(y2[n],y1), z=c(z2[n],z1), sequential = FALSE) * fac
       dists[lon1==lon2[n] & lat1==lat2[n]] = 0
       if (exclude.zero) {dists[dists==0] = NA}
       funres21.dist[n] = fun(dists,na.rm=TRUE)
--- R/sl.gc.dist.R
+++ R/sl.gc.dist.R
@@ -1,44 +1,114 @@
 sl.gc.dist <-
-  function(lon,lat,Rsphere=1,sequential=TRUE) {
+  function(lon=NULL,lat=NULL,x=NULL,y=NULL,z=NULL,Rsphere=1,sequential=TRUE,distmat=FALSE,distmat.split.index=NULL,byrot=FALSE) {
     
-    if (is.vector(lon) || length(dim(lon)) == 1) {
-      
-      if (sequential) {
-        
+    if (!byrot) {
+      if (is.null(x)) {
+        if (is.null(lon)) {stop("Either 'lon,lat' or 'x,y,z' must be provided")}
+        lon = pi / 180 * lon
+        lat = pi / 180 * lat
+        singlevec = (is.vector(lon) || length(dim(lon)) == 1)
+        if (singlevec) {
+          xyz.mat = cbind(cos(lat)*cos(lon),cos(lat)*sin(lon),sin(lat))
+        } else {
+          if (distmat) {stop("'distmat=TRUE' works only with a single sequence of points as input")}
+          xyz.mat = cbind(cos(lat[,1])*cos(lon[,1]),cos(lat[,1])*sin(lon[,1]),sin(lat[,1]))
+          xyz.mat.2 = cbind(cos(lat[,2])*cos(lon[,2]),cos(lat[,2])*sin(lon[,2]),sin(lat[,2]))
+        }
+      } else {
+        singlevec = (is.vector(x) || length(dim(x)) == 1)
+        if (singlevec) {
+          xyz.mat = cbind(x,y,z)
+        } else {
+          if (distmat) {stop("'distmat=TRUE' works only with a single sequence of points as input")}
+          xyz.mat = cbind(x[,1],y[,1],z[,1])
+          xyz.mat.2 = cbind(x[,2],y[,2],z[,2])
+        }
+      }
+      L = nrow(xyz.mat) - 1
+    } else {
+      if (is.null(lon)) {
+        if (is.null(x)) {stop("Either 'lon,lat' or 'x,y,z' must be provided")}
+        lola = sl.xyz2lonlat(x=x,y=y,z=z)
+        lon = lola$lon
+        lat = lola$lat
+      }
+      if (distmat) {stop("'distmat=TRUE' works only with 'byrot=FALSE'")}
+      singlevec = (is.vector(lon) || length(dim(lon)) == 1)
+      if (singlevec) {
         L = length(lon) - 1
-        pointdist = rep(NA,L)
-        for (l in 1:L) {
-          alpha = lon[l] + 90
-          beta = 90 - lat[l]
-          gamma = 0
-          rot.res = sl.rot(lon[l+1],lat[l+1],alpha,beta,gamma)
-          pointdist[l] = (90 - rot.res$lat) * pi * Rsphere / 180
-        } 
+      } else {
+        L = nrow(lon)
+      }
+    }
+    
+    if (!distmat) {
+      
+      if (singlevec) {
+        
+        if (sequential) {
+          
+          if (byrot) {
+            pointdist = rep(NA,L)
+            for (l in 1:L) {
+              alpha = lon[l] + 90
+              beta = 90 - lat[l]
+              gamma = 0
+              rot.res = sl.rot(lon[l+1],lat[l+1],alpha,beta,gamma)
+              pointdist[l] = (90 - rot.res$lat) * pi * Rsphere / 180
+            }
+          } else {
+            if (L==1) {
+              pointdist = (2*Rsphere)*asin(sqrt(sum((xyz.mat[2,]-xyz.mat[1,])^2))/2)
+            } else {
+              pointdist = (2*Rsphere)*asin(sqrt(rowSums((xyz.mat[2:(L+1),]-xyz.mat[1:L,])^2))/2)
+            }
+          }
+          
+        } else {
+          
+          if (byrot) {
+            alpha = lon[1] + 90
+            beta = 90 - lat[1]
+            gamma = 0
+            rot.res = sl.rot(lon[2:(L+1)],lat[2:(L+1)],alpha,beta,gamma)
+            pointdist = (90 - rot.res$lat) * pi * Rsphere / 180
+          } else {
+            if (L==1) {
+              pointdist = (2*Rsphere)*asin(sqrt(sum((xyz.mat[2,]-xyz.mat[1,])^2))/2)
+            } else {
+              pointdist = (2*Rsphere)*asin(sqrt(rowSums((xyz.mat[2:(L+1),]-
+                                                         matrix(xyz.mat[1,],ncol=3,nrow=L,byrow=TRUE))^2))/2)
+            }
+          }
+          
+        }
         
       } else {
         
-        L = length(lon) - 1
-        alpha = lon[1] + 90
-        beta = 90 - lat[1]
-        gamma = 0
-        rot.res = sl.rot(lon[2:(L+1)],lat[2:(L+1)],alpha,beta,gamma)
-        pointdist = (90 - rot.res$lat) * pi * Rsphere / 180
+        if (byrot) {
+          if (ncol(lon) != 2) {stop("With 'byrot=TRUE', if 'lon' and 'lat' are no vectors,
+	                              they must be 2-column matrices or data frames")}
+          pointdist = rep(NA,L)
+          for (l in 1:L) {
+            alpha = lon[l,1] + 90
+            beta = 90 - lat[l,1]
+            gamma = 0
+            rot.res = sl.rot(lon[l,2],lat[l,2],alpha,beta,gamma)
+            pointdist[l] = (90 - rot.res$lat) * pi * Rsphere / 180
+          }
+        } else {
+          if (L==1) {
+            pointdist = (2*Rsphere)*asin(sqrt(sum((xyz.mat-xyz.mat.2)^2))/2)
+          } else {
+            pointdist = (2*Rsphere)*asin(sqrt(rowSums((xyz.mat-xyz.mat.2)^2))/2)
+          }
+        }
         
       }
-      
+    
     } else {
       
-      if (ncol(lon) != 2) {stop("If 'lon' and 'lat' are no vectors,
-	                              they must be 2-column matrices or data frames")}
-      L = length(lon[,1])
-      pointdist = rep(NA,L)
-      for (l in 1:L) {
-        alpha = lon[l,1] + 90
-        beta = 90 - lat[l,1]
-        gamma = 0
-        rot.res = sl.rot(lon[l,2],lat[l,2],alpha,beta,gamma)
-        pointdist[l] = (90 - rot.res$lat) * pi * Rsphere / 180
-      }
+      pointdist = as.matrix((2*Rsphere)*asin(dist(xyz.mat)/2))
       
     }
     
